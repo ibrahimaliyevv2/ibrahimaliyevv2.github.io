61511153286030cabedc5070c2d70b5be393fd84
ibrahimaliyevv2 <7caucb@code.edu.az>
2025-11-03 01:43:00 +0400

    replaced deprecated libraries

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import './contact.css'
-import { MdOutlineEmail } from 'react-icons/md'
-import { RiMessengerLine } from 'react-icons/ri'
-import { BsWhatsapp } from 'react-icons/bs'
-import { useRef } from 'react'
-import emailjs from 'emailjs-com'
+import React from "react";
+import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
   const form = useRef();
@@ -12,51 +12,90 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_dxou8id', 'template_mh6u2bv', form.current, '0lDUE3VJ78AJH9FlU')
-      .then((result) => {
-        console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
-      });
+    emailjs
+      .sendForm(
+        "service_dxou8id",
+        "template_mh6u2bv",
+        form.current,
+        "0lDUE3VJ78AJH9FlU"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
 
     e.target.reset();
   };
 
   return (
-    <section id='contact'>
+    <section id="contact">
       <h5>Get In Touch</h5>
       <h2>Contact Me</h2>
 
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail className='contact__option-icon' />
+            <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
             <h5>ibrahimaliyevv2@gmail.com</h5>
-            <a href="mailto:ibrahimaliyevv2@gmail.com" target="_blank" rel="noopener noreferrer">Send an email</a>
+            <a
+              href="mailto:ibrahimaliyevv2@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Send an email
+            </a>
           </article>
           <article className="contact__option">
-            <RiMessengerLine className='contact__option-icon' />
+            <RiMessengerLine className="contact__option-icon" />
             <h4>Messenger</h4>
             <h5>Ibrahim Aliyev</h5>
-            <a href="https://m.me/ibrahimaliyevv" target="_blank" rel="noopener noreferrer">Send a message</a>
+            <a
+              href="https://m.me/ibrahimaliyevv"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Send a message
+            </a>
           </article>
           <article className="contact__option">
-            <BsWhatsapp className='contact__option-icon' />
+            <BsWhatsapp className="contact__option-icon" />
             <h4>WhatsApp</h4>
             <h5>+994 55 792 58 03</h5>
-            <a href="https://api.whatsapp.com/send/?phone=%2B994557925803" target="_blank" rel="noopener noreferrer">Send a message</a>
+            <a
+              href="https://api.whatsapp.com/send/?phone=%2B994557925803"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Send a message
+            </a>
           </article>
         </div>
         <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name='name' placeholder='Your Full Name' required />
-          <input type="email" name='email' placeholder='Your Email' required />
-          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
-          <button type='submit' className='btn btn-primary'>Send Message</button>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
